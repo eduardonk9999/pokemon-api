f256678382891a4d51886c2c4e1d12a0bb936694
eduardonk9999 <thiago.novak7@gmail.com>
2025-05-15 10:46:03 -0300

    service

---
+++ src/main/java/com/pokedex/service/pokemonService.java
@@ -0,0 +1,32 @@
+package com.pokedex.service;
+
+import com.pokedex.entity.Pokemon;
+import com.pokedex.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class pokemonService {
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    public List<Pokemon> findAll() {
+        return pokemonRepository.findAll();
+    }
+
+    public Optional<Pokemon> findById(Long id) {
+        return pokemonRepository.findById(id);
+    }
+
+    public Pokemon save(Pokemon pokemon) {
+        return pokemonRepository.save(pokemon);
+    }
+
+    public void delete(Long id) {
+        pokemonRepository.deleteById(id);
+    }
+
+}
---
+++ src/main/java/com/pokedex/service/treinadorService.java
@@ -0,0 +1,8 @@
+package com.pokedex.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class treinadorService {
+
+}
