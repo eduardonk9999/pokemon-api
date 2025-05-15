ede1efe5a7e54daf5f2071356442b6bd794e332e
eduardonk9999 <thiago.novak7@gmail.com>
2025-05-15 17:28:43 -0300

    entity

--- src/main/java/com/pokedex/service/treinadorService.java
+++ src/main/java/com/pokedex/service/treinadorService.java
@@ -1,8 +1,31 @@
 package com.pokedex.service;
 
+import com.pokedex.entity.Treinador;
+import com.pokedex.repository.TreinadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class treinadorService {
+    @Autowired
+    private TreinadorRepository treinadorRepository;
 
+    public List<Treinador> findAll() {
+        return treinadorRepository.findAll();
+    }
+
+    public Optional<Treinador> findById(Long id) {
+        return treinadorRepository.findById(id);
+    }
+
+    public Treinador save(Treinador treinador) {
+        return treinadorRepository.save(treinador);
+    }
+
+    public void delete(Long id) {
+        treinadorRepository.deleteById(id);
+    }
 }
